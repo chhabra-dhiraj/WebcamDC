4ff9848a40d0aa601001e644ecf5edc5990d39df
Dhiraj Chhabra <dhiraj.chhabra.g@gmail.com>
2024-11-04 12:03:41 +0530

    established proper socket connection

--- app/src/main/java/io/github/chhabra_dhiraj/webcamdc/MainActivity.kt
+++ app/src/main/java/io/github/chhabra_dhiraj/webcamdc/MainActivity.kt
@@ -60,48 +60,28 @@ class MainActivity : AppCompatActivity() {
 
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-        bindCameraUseCases()
-
         cameraProviderFuture.addListener({
-            // Used to bind the lifecycle of cameras to the lifecycle owner
             cameraProvider = cameraProviderFuture.get()
-            val provider = cameraProvider ?: return@addListener
-
-            // Preview
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.surfaceProvider = findViewById<PreviewView>(R.id.viewFinder).surfaceProvider
-                }
-
-            // Select back camera as a default
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                // Unbind use cases before rebinding
-                provider.unbindAll()
-
-                // Bind use cases to camera
-                provider.bindToLifecycle(
-                    this, cameraSelector, preview
-                )
-
-            } catch (exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
-            }
-
+            bindCameraUseCases()
         }, ContextCompat.getMainExecutor(this))
     }
 
     private fun bindCameraUseCases() {
         val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-        val preview = Preview.Builder().build()
+        val preview = Preview.Builder()
+            .build()
+            .also {
+                it.surfaceProvider = findViewById<PreviewView>(R.id.viewFinder).surfaceProvider
+            }
         val imageAnalysis = ImageAnalysis.Builder()
             .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
             .build()
 
+        Log.e(TAG, "Dhiraj socket 1 connected")
+
         imageAnalysis.setAnalyzer(cameraExecutor) { image ->
             try {
+                Log.e(TAG, "Dhiraj socket 2 connected")
                 encodeAndSendFrame(image)
             } catch (e: Exception) {
                 Log.e("MainActivity", "Error encoding or sending frame", e)
@@ -114,61 +94,65 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun encodeAndSendFrame(image: ImageProxy) {
-        // 1. Initialize MediaCodec if not already initialized
-        val codec: MediaCodec
-        if (mediaCodec == null) {
-            mediaCodec = MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_VIDEO_AVC)
-            val format = MediaFormat.createVideoFormat(
-                MediaFormat.MIMETYPE_VIDEO_AVC,
-                image.width,
-                image.height
-            )
-            codec = mediaCodec ?: return
-            // Configure MediaFormat with desired encoding parameters
-            codec.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE)
-            codec.start()
-        } else {
-            codec = mediaCodec ?: return
-        }
-
-        // 2. Get image data
-        val buffer = image.planes[0].buffer
-        val bufferSize = buffer.remaining()
-        val data = ByteArray(bufferSize)
-        buffer.get(data)
-
-        // 3. Encode frame
-        val inputBufferIndex = codec.dequeueInputBuffer(TIMEOUT_US)
-        if (inputBufferIndex >= 0) {
-            val inputBuffer = codec.getInputBuffer(inputBufferIndex)
-            inputBuffer?.put(data)
-            codec.queueInputBuffer(
-                inputBufferIndex,
-                0,
-                bufferSize,
-                image.imageInfo.timestamp,
-                0
-            )
-        }
-
-        // 4. Send encoded data over socket
-        val bufferInfo = MediaCodec.BufferInfo()
-        val outputBufferIndex = codec.dequeueOutputBuffer(bufferInfo, TIMEOUT_US)
-        if (outputBufferIndex >= 0) {
-            val outputBuffer = codec.getOutputBuffer(outputBufferIndex)
-            val encodedData = ByteArray(bufferInfo.size)
-            outputBuffer?.get(encodedData)
+        Log.e(TAG, "Dhiraj socket 3 connected")
+//        // 1. Initialize MediaCodec if not already initialized
+//        if (mediaCodec == null) {
+//            mediaCodec = MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_VIDEO_AVC)
+//            val format = MediaFormat.createVideoFormat(
+//                MediaFormat.MIMETYPE_VIDEO_AVC,
+//                image.width,
+//                image.height
+//            )
+//            // Configure MediaFormat with desired encoding parameters
+//            mediaCodec?.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE)
+//            mediaCodec?.start()
+//        }
+//
+//        // 2. Get image data
+//        val buffer = image.planes[0].buffer
+//        val bufferSize = buffer.remaining()
+//        val data = ByteArray(bufferSize)
+//        buffer.get(data)
+//
+//        // 3. Encode frame
+//        val inputBufferIndex = mediaCodec?.dequeueInputBuffer(TIMEOUT_US) ?: 0
+//        Log.e(TAG, "Dhiraj socket 4 " +
+//                "connected inputBufferIndex: $inputBufferIndex")
+//        if (inputBufferIndex >= 0) {
+//            val inputBuffer = mediaCodec?.getInputBuffer(inputBufferIndex)
+//            inputBuffer?.put(data)
+//            mediaCodec?.queueInputBuffer(
+//                inputBufferIndex,
+//                0,
+//                bufferSize,
+//                image.imageInfo.timestamp,
+//                0
+//            )
+//        }
+//
+//        // 4. Send encoded data over socket
+//        val bufferInfo = MediaCodec.BufferInfo()
+//        val outputBufferIndex = mediaCodec?.dequeueOutputBuffer(bufferInfo, TIMEOUT_US) ?: 0
+//        Log.e(TAG, "Dhiraj socket 5 " +
+//                "connected outputBufferIndex: $outputBufferIndex")
+//        if (outputBufferIndex >= 0) {
+//            val outputBuffer = mediaCodec?.getOutputBuffer(outputBufferIndex)
+//            val encodedData = ByteArray(bufferInfo.size)
+//            outputBuffer?.get(encodedData)
 
             // Send encodedData over socket
             if (socket == null || socket?.isClosed == true) {
                 socket = Socket("192.168.0.107", 8080)
             }
+                Log.e(TAG, "Dhiraj socket 6 " +
+                        "connected")
             val lSocket = socket ?: return
 
-            lSocket.outputStream.write(encodedData)
+            lSocket.outputStream.write(24)
+//            lSocket.outputStream.write(encodedData)
 
-            mediaCodec?.releaseOutputBuffer(outputBufferIndex, false)
-        }
+//            mediaCodec?.releaseOutputBuffer(outputBufferIndex, false)
+//        }
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
